ded6b9e76c841d36e8e0bba731d0a497b5f169df
Alex Koziol <alekoz47@gmail.com>
2017-01-05 11:13:16 -0500

    replace define-struct with struct

--- pong.rkt
+++ pong.rkt
@@ -37,13 +37,13 @@
 ;;================================
 ;;Data definitions:
 
-(define-struct element (pos vel speed))
-;;Element is (make-element Posn Posn Integer)
+(struct element (pos vel speed))
+;;Element is (element Posn Posn Integer)
 ;;Assume:
 ;;- Vel Posn -1 -> 1
 
-(define-struct world (ball paddle1 paddle2))
-;;World is (make-world Element Element Element Posn)
+(struct world (ball paddle1 paddle2))
+;;World is (world Element Element Element Posn)
 ;;Assume:
 ;;- Paddle1 && Paddle2 Pos X fixed
 ;;- Paddle1 && Paddle2 Speed fixed
@@ -63,10 +63,10 @@
 
 ;;World -> World
 (define (tock w)
-  (make-world (move-ball (world-ball w)
-                         (choose-paddle w))
-              (move-paddle (world-paddle1 w))
-              (move-paddle (world-paddle2 w))))
+  (world (move-ball (world-ball w)
+                    (choose-paddle w))
+         (move-paddle (world-paddle1 w))
+         (move-paddle (world-paddle2 w))))
 
 ;;World -> Element
 (define (choose-paddle w)
@@ -98,12 +98,12 @@
                (+ (posn-y (element-pos p))
                   (/ (image-height PADDLE) 2) PADDING)))
       (advance-element
-       (make-element (element-pos b)
+       (element (element-pos b)
                      (make-posn (- 0 (posn-x (element-vel b)))
                                 (* (bounce-factor p) 
                                    (posn-y (element-vel b))))
                      (* PADDLE-FACTOR (element-speed b))))
-      (make-element (make-posn (/ WIDTH 2)
+      (element (make-posn (/ WIDTH 2)
                                (+ (/ HEIGHT 4) (/ (random HEIGHT) 2)))
                     (reset-vel p)
                     BALL-SPEED)))
@@ -125,14 +125,14 @@
 ;;Element -> Element
 (define (up/down-ball b)
   (advance-element
-   (make-element (element-pos b)
+   (element (element-pos b)
                  (make-posn (posn-x (element-vel b))
                             (- 0 (posn-y (element-vel b))))
                  (element-speed b))))
 
 ;;Element -> Element
 (define (advance-element e)
-  (make-element (make-posn (+ (posn-x (element-pos e))
+  (element (make-posn (+ (posn-x (element-pos e))
                               (* (element-speed e)
                                  (posn-x (element-vel e))))
                            (- (posn-y (element-pos e))
@@ -149,7 +149,7 @@
                (posn-y (element-pos p))))
       (advance-element p)
       (advance-element
-       (make-element (element-pos p)
+       (element (element-pos p)
                      (make-posn (posn-x (element-vel p))
                                 (- 0 (* 2 (posn-y (element-vel p)))))
                      (element-speed p)))))
@@ -165,11 +165,11 @@
 ;;World KeyEvent -> World
 (define (handle-key w ke)
   (cond ((or (key=? ke "w") (key=? ke "s"))
-         (make-world (world-ball w)
+         (world (world-ball w)
                      (set-paddle ke (world-paddle1 w))
                      (world-paddle2 w)))
         ((or (key=? ke "up") (key=? ke "down"))
-         (make-world (world-ball w)
+         (world (world-ball w)
                      (world-paddle1 w)
                      (set-paddle ke (world-paddle2 w))))
         (else w)))
@@ -177,21 +177,21 @@
 ;;KeyEvent Paddle -> Paddle
 (define (set-paddle ke p)
   (if (or (key=? ke "w") (key=? ke "up"))
-      (make-element (element-pos p)
+      (element (element-pos p)
                     (make-posn 0 1)
                     (element-speed p))
-      (make-element (element-pos p)
+      (element (element-pos p)
                     (make-posn 0 -1)
                     (element-speed p))))
 
 ;;World KeyEvent -> World
 (define (handle-release w ke)
   (cond ((or (key=? ke "w") (key=? ke "s"))
-         (make-world (world-ball w)
+         (world (world-ball w)
                      (reset-paddle ke (world-paddle1 w))
                      (world-paddle2 w)))
         ((or (key=? ke "up") (key=? ke "down"))
-         (make-world (world-ball w)
+         (world (world-ball w)
                      (world-paddle1 w)
                      (reset-paddle ke (world-paddle2 w))))
         (else w)))
@@ -199,10 +199,10 @@
 ;;KeyEvent Paddle -> Paddle
 (define (reset-paddle ke p)
   (if (or (key=? ke "w") (key=? ke "up"))
-      (make-element (element-pos p)
+      (element (element-pos p)
                     (make-posn 0 0)
                     (element-speed p))
-      (make-element (element-pos p)
+      (element (element-pos p)
                     (make-posn 0 0)
                     (element-speed p))))
 
@@ -210,15 +210,15 @@
 ;;Run:
 
 (main
- (make-world
-  (make-element (make-posn (/ WIDTH 2) (/ HEIGHT 2))
+ (world
+  (element (make-posn (/ WIDTH 2) (/ HEIGHT 2))
                 (make-posn 0.701 -0.701)
                 BALL-SPEED)
-  (make-element (make-posn (* (/ 3 2) PADDING)
+  (element (make-posn (* (/ 3 2) PADDING)
                            (/ HEIGHT 2))
                 (make-posn 0 0)
                 PADDLE-SPEED)
-  (make-element (make-posn (- WIDTH
+  (element (make-posn (- WIDTH
                               (* (/ 3 2) PADDING))
                            (/ HEIGHT 2))
                 (make-posn 0 0)
